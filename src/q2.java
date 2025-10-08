02afd5e8f5b8487b483694dfc85180daee51b3c7
kaio <kaio22barra@gmail.com>
2025-10-08 12:26:38 -0300

    Update q2.java

--- src/q2.java
+++ src/q2.java
@@ -1,4 +1,47 @@
-package PACKAGE_NAME;
+package pessoas;
 
-public class q {
+public class Fornecedor extends Pessoa {
+    private double valorCredito;
+    private double valorDivida;
+
+    public Fornecedor() {
+        super();
+        this.valorCredito = 0;
+        this.valorDivida = 0;
+    }
+
+    public Fornecedor(String nome, String endereco, String telefone,
+                      double valorCredito, double valorDivida) {
+        super(nome, endereco, telefone);
+        this.valorCredito = valorCredito;
+        this.valorDivida = valorDivida;
+    }
+
+    public double getValorCredito() { return valorCredito; }
+    public void setValorCredito(double valorCredito) { this.valorCredito = valorCredito; }
+
+    public double getValorDivida() { return valorDivida; }
+    public void setValorDivida(double valorDivida) { this.valorDivida = valorDivida; }
+
+    public double obterSaldo() {
+        return valorCredito - valorDivida;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", Valor Crédito: " + valorCredito +
+                ", Valor Dívida: " + valorDivida +
+                ", Saldo: " + obterSaldo();
+    }
 }
+
+//teste
+
+package pessoas;
+
+public class TesteFornecedor {
+    public static void main(String[] args) {
+        Fornecedor f = new Fornecedor("McDonald's", "Rod. Augusto Montenegro", "(91) 4002-8922", 10000, 2500);
+        System.out.println(f);
+    }
